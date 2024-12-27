8b259026699cd461b8b55e54d174f3c3a5fecf6c
Alessandro Veloso <157850150+wAlessandro@users.noreply.github.com>
2024-12-27 12:05:17 -0300

    Delete FrogHunter directory

--- FrogHunter/objects/__init__.py
+++
@@ -1,1 +0,0 @@
-from objects.background import *
--- FrogHunter/objects/background.py
+++
@@ -1,6 +0,0 @@
-from tkinter import PhotoImage, Canvas
-
-class Background:
-    def __init__(self,canvas:Canvas, image:PhotoImage) -> None:
-        self.canvas = canvas
-        
